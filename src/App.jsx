be2e38d85b2c689571e9573ee7809b980de5f3d7
OMR <cartoon6676@gmail.com>
2026-03-21 15:51:02 +0200

    handling project creation

--- src/App.jsx
+++ src/App.jsx
@@ -21,12 +21,14 @@ function App() {
 
   function handleAddProject(projectData) {
     setProjectsState((prevState) => {
+      const projectId = Math.random();
       const newProject = {
         ...projectData,
-        id: Math.random(),
+        id: projectId,
       };
       return {
         ...prevState,
+        selectedProjectID: undefined,
         projects: [...prevState.projects, newProject],
       };
     });
@@ -43,7 +45,10 @@ function App() {
   return (
     <>
       <main className="h-screen my-8 flex gap-8">
-        <ProjectSidebar onStartAddProject={handleStartAddProject} />
+        <ProjectSidebar
+          projects={projectsState.projects}
+          onStartAddProject={handleStartAddProject}
+        />
         {content}
       </main>
     </>
